ea9a1223f5fbadfbe1dcad2080b10797098c5969
Joel Filho <joelfilho02812@gmail.com>
2022-12-29 10:59:57 -0300

    add: styling products lsit page

--- src/components/ListProducts.vue
+++ src/components/ListProducts.vue
@@ -1,10 +1,18 @@
 <template>
   <section class="products-container">
-    <div v-for="product in products" :key="product.id">
-        <img v-if="product.fotos" :src="product.fotos[0].src" :alt="product.fotos[0]">
-        <h2 class="title">{{ product.nome }}</h2>
-        <p class="price">{{ product.preco }}</p>
-        <p class="description">{{ product.descricao }}</p>
+    <div v-if="products && products.length > 0" class="products">
+        <div v-for="product in products" :key="product.id" class="product">
+            <router-link to="/">
+                <img v-if="product.fotos" :src="product.fotos[0].src" :alt="product.fotos[0]">
+                <p class="price">{{ product.preco }}</p>
+                <h2 class="title">{{ product.nome }}</h2>
+                <p class="description">{{ product.descricao }}</p>
+            </router-link>
+        </div>
+    </div> 
+
+    <div v-else-if="products && products.length === 0">
+        <p class="without-results" >Search without results. Try another term.</p>
     </div>
   </section>
 </template>
@@ -52,6 +60,59 @@ export default Vue.extend({
 })
 </script>
 
-<style>
+<style scoped>
+
+.products-container {
+    max-width: 1000px;
+
+    margin: 0 auto;
+}
+
+.products {
+    display: grid;
+    grid-template-columns: repeat(3, 1fr);
+    grid-gap: 30px;
+
+    margin: 30px;
+}
+
+.product {
+    box-shadow: 0 4px 8px rgba(30, 60, 90, 0.1);
+
+    background: #fff;
+    
+    border-radius: 4px;
+
+    transition: all 0.2s;
+
+    padding: 10px;
+}
+
+.product:hover {
+    box-shadow: 0 6px 12px rgba(30, 60, 90, 0.2);
+
+    position: relative;
+    z-index: 1;
+
+    transform: scale(1.1);
+}
+
+.produto img {
+    border-radius: 4px;
+    margin-bottom: 20px;
+}
+
+.title {
+    margin-bottom: 10px;
+}
+
+.price {
+    color: #e80;
+    font-weight: bold;
+}
+
+.without-results {
+    text-align: center;
+}
 
 </style>
